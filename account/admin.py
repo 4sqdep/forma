d42fa4c10395ac96d03d120d092377eb283f6ac3
Erkin <erkin@MacBook-Pro-Erkin.local>
2024-11-06 17:57:49 +0500

    user model

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from .models import User, Department, Position, Permission, UserProfile
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', 'first_name', 'last_name', 'created_at']
+    list_display_links = ['id', 'username', 'first_name', 'last_name']
+    search_fields = ['username', 'first_name']
+
+
+admin.site.register(User, UserAdmin)
+
+
+class PositionAdminInline(admin.TabularInline):
+    model = Position
+    extra = 1
+    fields = ['id', 'name', 'created_at']
+    readonly_fields = ['created_at']
+    can_delete = True
+
+
+class DepartmentAdmin(admin.ModelAdmin):
+    inlines = [PositionAdminInline]
+    list_display = ['id', 'name', 'created_at']
+    list_display_links = ['id', 'name']
+    search_fields = ['name']
+
+
+admin.site.register(Department, DepartmentAdmin)
+
+
+class PermissionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'position', 'can_get', 'can_post', 'can_patch', 'can_put', 'can_delete', 'created_at']
+    list_display_links = ['id', 'position', 'can_get']
+    search_fields = ['position']
+
+
+admin.site.register(Permission, PermissionAdmin)
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'department', 'position', 'created_at']
+    list_display_links = ['id', 'user', 'department', 'position']
+    search_fields = ['department']
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
--- account/models.py
+++ account/models.py
@@ -1,3 +1,71 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    is_download = models.BooleanField(default=False, verbose_name="Yuklash")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Kiritilgan vaqti")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Uzgargan vaqti")
+    def __str__(self):
+        return self.username
+    class Meta:
+        verbose_name = "Foydalanuvchi"
+        verbose_name_plural = "Foydalanuvchilar"
+
+
+
+# Bo'limlar uchun model
+class Department(models.Model):
+    name = models.CharField(max_length=50, unique=True, null=False, blank=False, verbose_name="Nomi")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Kiritilgan vaqti")
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name = "Bo'lim"
+        verbose_name_plural = "Bo'limlar"
+
+
+# Lavozim modeli
+class Position(models.Model):
+    name = models.CharField(max_length=50, unique=True, null=False, blank=False, verbose_name="Nomi")
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True, blank=True, verbose_name="Bo'lim")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Kiritilgan vaqti")
+    def __str__(self):
+        return f"{self.department} - {self.name}"
+    class Meta:
+        verbose_name = "Lavozim"
+        verbose_name_plural = "Lavozimlar"
+
+
+# Ruxsatlar modelini yaratamiz (bu model lavozimga ko'ra metodlarga ruxsat beradi)
+class Permission(models.Model):
+    position = models.OneToOneField(Position, on_delete=models.SET_NULL, related_name='permission',
+                                    null=True, blank=True, verbose_name="Bo'lim")
+    can_get = models.BooleanField(default=False, verbose_name="GET")
+    can_post = models.BooleanField(default=False, verbose_name="POST")
+    can_patch = models.BooleanField(default=False, verbose_name="PATCH")
+    can_put = models.BooleanField(default=False, verbose_name="PUT")
+    can_delete = models.BooleanField(default=False, verbose_name="DELETE")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Kiritilgan vaqti")
+    def __str__(self):
+        return f"Ruxsatnomalar uchun - {self.position.name}"
+
+    class Meta:
+        verbose_name = "Ruxsat"
+        verbose_name_plural = "Ruxsatnomalar"
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.SET_NULL, related_name="profile", verbose_name="Foydalanuvchi",)
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True, blank=True, verbose_name="Bo'lim")
+    position = models.ForeignKey(Position, on_delete=models.SET_NULL, null=True, blank=True, verbose_name="Lavozim")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Kiritilgan vaqti")
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        verbose_name = "Profil"
+        verbose_name_plural = "Profillar"
+
+
