77ea2bb255a730bbf05e3971b3ae33b482f9210e
Erkin <erkin@MacBook-Pro-Erkin.local>
2024-11-06 21:05:32 +0500

    urls

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,9 @@ from .yasg import urlpatterns as doc_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include([
+        path('', include('account.urls'))
+    ]))
 ]
 
 if settings.DEBUG:
