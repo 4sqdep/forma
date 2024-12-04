e089bb2eaf348e8b7ce428e7dbfc20a26765406f
4sqdep <4.sqdep@gmail.com>
2024-12-04 12:29:12 +0500

    Delete Swagger

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .yasg import urlpatterns as doc_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -17,4 +16,3 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-urlpatterns += doc_urls
