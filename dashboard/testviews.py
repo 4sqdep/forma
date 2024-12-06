4edb889539131d5f54f8b5715ba0aff51d802ccd
4sqdep <4.sqdep@gmail.com>
2024-12-06 11:58:16 +0500

    Subquery

--- dashboard/testviews.py
+++ dashboard/testviews.py
@@ -1,56 +1,42 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.db.models import Exists, OuterRef, Prefetch
+from django.db.models import Exists, OuterRef, Prefetch, Subquery
 from rest_framework import status
 from .models import DashboardButton, DashboardCategoryButton, DashboardSubCategoryButton
 
+
 class DashboardButtonListView(APIView):
     def get(self, request, *args, **kwargs):
-        # Annotatsiya subquery orqali subkategoriyalarni tekshirish
-        subcategory_exists = DashboardSubCategoryButton.objects.filter(
-            dashboard_category_btn=OuterRef('pk')
-        )
-
-        # Annotatsiya kategoriyalarda subkategoriya borligini aniqlash uchun
-        categories_with_subcategories = DashboardCategoryButton.objects.annotate(
-            has_data=Exists(subcategory_exists)
-        ).prefetch_related(
-            Prefetch(
-                'dashboardsubcategorybutton_set',
-                queryset=DashboardSubCategoryButton.objects.all(),
-                to_attr='subcategories'
+        buttons_with_categories = DashboardButton.objects.prefetch_related(
+            'dashboardcategorybutton_set',
+            queryset=DashboardCategoryButton.objects.annotate(
+                has_data=Subquery(
+                    DashboardSubCategoryButton.objects.filter(
+                        dashboard_category_btn=OuterRef('pk')
+                    ).exists()
+                )
+            ).prefetch_related(
+                'subcategories',
+                queryset=DashboardSubCategoryButton.objects.all()
             )
         )
 
         response_data = []
-
-        # Har bir asosiy tugma uchun ma'lumotlarni yig'ish
-        for button in DashboardButton.objects.prefetch_related('dashboardcategorybutton_set'):
-            categories = categories_with_subcategories.filter(dashboard_button=button)
+        for button in buttons_with_categories:
             categories_data = []
-            button_has_data = False
-
-            for category in categories:
-                subcategories = category.subcategories
-                category_has_data = category.has_data or len(subcategories) > 0
-
-                if category_has_data:
-                    button_has_data = True
-
+            for category in button.dashboardcategorybutton_set.all():
                 categories_data.append({
                     "id": category.id,
                     "name": category.name,
-                    "has_data": category_has_data,
+                    "has_data": category.has_data or len(category.subcategories) > 0,
                     "subcategories": [
-                        {"id": sub.id, "name": sub.name} for sub in subcategories
+                        {"id": sub.id, "name": sub.name} for sub in category.subcategories
                     ]
                 })
-
-            # Asosiy tugma uchun ma'lumot
             response_data.append({
                 "id": button.id,
                 "name": button.name,
-                "has_data": button_has_data,
+                "has_data": any(category['has_data'] for category in categories_data),
                 "categories": categories_data
             })
 
