39e98c7230e5ce48e9e0c05990e71ab053bcd752
4sqdep <4.sqdep@gmail.com>
2024-12-07 16:28:47 +0500

    yana test

--- dashboard/signals.py
+++ dashboard/signals.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 def create_project_documentation(sender, instance, created, **kwargs):
     """DashboardSubCategoryButton yaratilganda 3 ta ProjectDocumentation qo'shish"""
     if created:  # Faqat yangi ma'lumot qo'shilganda ishlaydi
-        names = ["Qurilish ishlari", "Montaj ishlari", "Uskunalar"]  # 3 ta hujjat nomi
+        names = ["Loyiha hujjatlari", "Qurilish montaj ishlari hujjatlari", "Uskunalar hujjatlari"]  # 3 ta hujjat nomi
         for name in names:
             ProjectDocumentation.objects.create(
                 subcategories_btn=instance,  # Bog'laymiz
