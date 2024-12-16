976980d755cd4c2502a087b8e5e5ed02f3912577
4sqdep <4.sqdep@gmail.com>
2024-12-16 15:30:56 +0500

    str

--- reestr/models.py
+++ reestr/models.py
@@ -4,7 +4,7 @@ class Nocapitelni(models.Model):
     file = models.FileField(upload_to='nocapitelni', null=True, blank=True, verbose_name='Fayl')
 
     def __str__(self):
-        return self.file
+        return str(self.file)
     class Meta:
         verbose_name = 'Nocapitelni'
         verbose_name_plural = 'Nocapitelni'
