ef5cc5a5508fe2ab4c7bec6d1aeaa6c8ed083a80
4sqdep <4.sqdep@gmail.com>
2024-12-23 22:59:29 +0500

    adminsortable2

--- config/settings.py
+++ config/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'adminsortable2',
     'corsheaders',
     'drf_yasg',
     'rest_framework',
