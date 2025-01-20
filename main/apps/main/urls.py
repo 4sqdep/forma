c1655a5e311f7c134c2138bc15b075221255e110
4sqdep <4.sqdep@gmail.com>
2025-01-20 17:44:20 +0500

    patch object password

--- main/apps/main/urls.py
+++ main/apps/main/urls.py
@@ -7,6 +7,8 @@ app_name = "main"
 
 urlpatterns = [
     path('get/objects/password/<int:pk>/', GetObjectsPasswordView.as_view(), name='get-objects-password'),
+    path('post-objects-password/', GetObjectsPasswordView.as_view(), name='post-objects-password'),
+    path('patch-objects-password/<int:pk>/', GetObjectsPasswordView.as_view(), name='patch-objects-password'),
     path('upload-file/', UploadFilesAPIView.as_view(), name='upload-file'),
     path('get-file/<int:pk>/', GetFilesAPIView.as_view(), name='get-file'),
 ]
