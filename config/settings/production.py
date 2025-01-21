e93ab7833203070960781fe60c72c615756ef892
Direksiya <4.sqdep@gmail.com>
2025-01-21 11:57:16 +0500

    new one

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,6 +1,6 @@
 from .base import *  # noqa
 
 # TODO: as soon as the project goes to production fix allowed hosts
-SERVER_IP = os.environ.get("SERVER_IP")
+SERVER_IP = os.environ.get("13.233.123.235")
 ALLOWED_HOSTS = ["*"]
 DEBUG = True
