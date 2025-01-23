db1febd152f0b557e76db06b79e014ba51ef1a69
4sqdep <4.sqdep@gmail.com>
2025-01-23 15:09:00 +0500

    devpush

--- main/apps/reestr/admin.py
+++ main/apps/reestr/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from main.apps.reestr.models.construction import MonthlyExpense, ConstructionTask, Year, Month
+from main.apps.reestr.models.currency import Currency
 
 
 
@@ -7,3 +8,4 @@ admin.site.register(ConstructionTask)
 admin.site.register(MonthlyExpense)
 admin.site.register(Year)
 admin.site.register(Month)
+admin.site.register(Currency)
