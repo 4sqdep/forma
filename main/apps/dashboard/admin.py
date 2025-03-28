ba24e3c422e2d4c8ab549c6b6d2400c92abf3e86
4sqdep <4.sqdep@gmail.com>
2025-03-28 12:10:55 +0500

    new pull request

--- main/apps/dashboard/admin.py
+++ main/apps/dashboard/admin.py
@@ -69,9 +69,41 @@ class ProjectSectionsAdmin(admin.ModelAdmin):
 
 admin.site.register(ProjectSections, ProjectSectionsAdmin)
 
+class AdminConstructionInstallationFile(admin.ModelAdmin):
+    list_display = ['id', 'section', 'title', 'date', 'file_code']
+    list_display_links = ['section', 'title', 'date', 'file_code']
+    search_fields = ['title', 'file_code']
 
-admin.site.register(ConstructionInstallationSection)
-admin.site.register(ConstructionInstallationFile)
-admin.site.register(ConstructionInstallationStatistics)
-admin.site.register(ConstructionInstallationProject)
-admin.site.register(MonthlyCompletedTask)
+admin.site.register(ConstructionInstallationFile, AdminConstructionInstallationFile)
+
+
+class AdminConstructionInstallationProject(admin.ModelAdmin):
+    list_display = ['id', 'section', 'title', 'currency', 'allocated_amount']
+    list_display_links = ['section', 'title', 'currency', 'allocated_amount']
+    search_fields = ['title']
+
+admin.site.register(ConstructionInstallationProject, AdminConstructionInstallationProject)
+
+
+class AdminConstructionInstallationSection(admin.ModelAdmin):
+    list_display = ['id', 'object', 'title', 'is_forma', 'is_file']
+    list_display_links = ['object', 'title', 'is_forma', 'is_file']
+    search_fields = ['title']
+
+admin.site.register(ConstructionInstallationSection, AdminConstructionInstallationSection)
+
+
+class AdminConstructionInstallationStatistics(admin.ModelAdmin):
+    list_display = ['id', 'object', 'contractor', 'installation_work_amount', 'remanied_work_amount', 'cost_of_performed_work', 'date']
+    list_display_links = ['object', 'contractor', 'installation_work_amount', 'remanied_work_amount', 'cost_of_performed_work', 'date']
+    search_fields = ['contractor']
+
+admin.site.register(ConstructionInstallationStatistics, AdminConstructionInstallationStatistics)
+
+
+class AdminMonthlyCompletedTask(admin.ModelAdmin):
+    list_display = ['id', 'construction_installation_project', 'date', 'monthly_amount']
+    list_display_links = ['construction_installation_project', 'date', 'monthly_amount']
+    search_fields = ['date']
+
+admin.site.register(MonthlyCompletedTask, AdminMonthlyCompletedTask)
