822d91d774d5b8932f2ee1cca31ec1cf6b58a825
Direksiya <4.sqdep@gmail.com>
2025-04-02 12:04:08 +0500

    new bug fix

--- main/apps/dashboard/admin.py
+++ main/apps/dashboard/admin.py
@@ -1,11 +1,4 @@
 from django.contrib import admin
-# from main.apps.dashboard.models.construction_installation_work import (
-#     ConstructionInstallationFile, 
-#     ConstructionInstallationSection, 
-#     ConstructionInstallationStatistics,
-#     ConstructionInstallationProject,
-#     MonthlyCompletedTask
-# )
 from main.apps.dashboard.models.dashboard import ObjectCategory, ObjectSubCategory
 
 
@@ -33,44 +26,3 @@ class ObjectAdmin(admin.ModelAdmin):
     list_display_links = ['title']
     search_fields = ['title']
 
-
-# admin.site.register(Object, ObjectAdmin)
-
-
-# class NextStageDocumentsAdmin(admin.ModelAdmin):
-#     list_display = ['id', 'created_by_full_name', 'object', 'name',
-#                     'is_file', 'is_forma', 'is_section']
-#     list_display_links = ['name']
-#     search_fields = ['name']
-
-
-#     def created_by_full_name(self, obj):
-#         if obj.created_by:  
-#             return f"{obj.created_by.first_name} {obj.created_by.last_name}"
-#         return "No user assigned"
-
-
-# admin.site.register(NextStageDocuments, NextStageDocumentsAdmin)
-
-
-# class DocumentFilesAdmin(admin.ModelAdmin):
-#     list_display = ['id', 'document', 'created_by', 'files']
-#     list_display_links = ['document', 'files']
-
-
-# admin.site.register(DocumentFiles)
-
-
-# class ProjectSectionsAdmin(admin.ModelAdmin):
-#     list_display = ['id', 'next_stage_documents', 'created_by', 'name']
-#     list_display_links = ['name']
-#     search_fields = ['name']
-
-# admin.site.register(ProjectSections, ProjectSectionsAdmin)
-
-
-# admin.site.register(ConstructionInstallationSection)
-# admin.site.register(ConstructionInstallationFile)
-# admin.site.register(ConstructionInstallationStatistics)
-# admin.site.register(ConstructionInstallationProject)
-# admin.site.register(MonthlyCompletedTask)
