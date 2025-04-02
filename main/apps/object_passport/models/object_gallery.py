1189be600f64e67f7a345b4148283e6d33d524f1
4sqdep <4.sqdep@gmail.com>
2025-04-02 18:05:57 +0500

    Object Gallery model create

---
+++ main/apps/object_passport/models/object_gallery.py
@@ -0,0 +1,29 @@
+import os
+from django.db import models
+from main.apps.common.models import BaseModel, BaseMeta
+from django.utils.timezone import now
+from main.apps.object_passport.models.object import Object
+
+
+
+def upload_object_files(instance, filename):
+    ext = os.path.splitext(filename)[1]
+    original_name = os.path.splitext(filename)[0]
+    timestamp = now().strftime("%Y_%m_%d")
+    return f"object_files/{original_name}_{timestamp}{ext}"
+
+
+class Gallery(BaseModel):
+    object = models.ForeignKey(Object, on_delete=models.SET_NULL, null=True, blank=True,
+                               verbose_name="Obyekt nomi")
+    file = models.FileField(upload_to=upload_object_files, blank=True, null=True, verbose_name="Rasm yoki Video")
+
+    name = models.CharField(max_length=100, blank=True, null=True, verbose_name="Nomi")
+
+    def __str__(self):
+        return f"{self.object.title}"
+
+    class Meta(BaseMeta):
+        db_table = "object_gallery"
+        verbose_name = "Object Gallery"
+        verbose_name_plural = "Objects Gallery"
---
+++ main/apps/object_passport/serializers/object_gallery.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from main.apps.object_passport.models.object_gallery import Gallery
+
+
+class ObjectGalleryCreateUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Gallery
+        fields = ('object', 'name', 'file')
