237edc2c4c0601159bf9ef5043a55fd464086946
4sqdep <4.sqdep@gmail.com>
2025-04-15 11:11:57 +0500

    serializer name update

--- main/apps/employee_communication/serializers.py
+++ main/apps/employee_communication/serializers.py
@@ -10,6 +10,10 @@ from main.apps.object_passport.serializers.object import ObjectTitleSerializer
 
 
 
+class BaseEmployeeCommunicationSerialize(serializers.ModelSerializer):
+    class Meta:
+        model = EmployeeCommunication
+        fields = ['id', 'title']
 
 
 
@@ -129,11 +133,6 @@ class TextMessageSerializer(serializers.ModelSerializer):
             'created_at'
         )
 
-class FilterEmployeeCommunicationSerialize(serializers.ModelSerializer):
-    class Meta:
-        model = EmployeeCommunication
-        fields = ['id', 'title']
-
 
 class ObjectWithCommunicationsSerializer(serializers.ModelSerializer):
     communications = serializers.SerializerMethodField()
@@ -145,7 +144,7 @@ class ObjectWithCommunicationsSerializer(serializers.ModelSerializer):
 
     def get_communications(self, obj):
         comms = EmployeeCommunication.objects.filter(obj=obj)
-        return FilterEmployeeCommunicationSerialize(comms, many=True).data
+        return BaseEmployeeCommunicationSerialize(comms, many=True).data
 
     def get_counts(self, obj):
         # 1. Belgilangan tartibda statuslar
