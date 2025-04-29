f1290a04653aedb4dabf6df62c46a8e5df3b5a76
Direksiya <4.sqdep@gmail.com>
2025-04-29 17:42:28 +0500

    new changess

--- main/apps/contract/serializers.py
+++ main/apps/contract/serializers.py
@@ -2,6 +2,11 @@ from rest_framework import serializers
 from main.apps.contract.models import ContractSection, ContractFile
 
 
+
+
+
+
+
 class ContractSectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = ContractSection
@@ -11,6 +16,19 @@ class ContractSectionSerializer(serializers.ModelSerializer):
 
 class ContractSectionFileSerializer(serializers.ModelSerializer):
     date = serializers.DateField(format="%d-%m-%Y", input_formats=["%Y-%m-%d"], required=False)
+    file_name = serializers.SerializerMethodField()
+
     class Meta:
         model = ContractFile
-        fields = ['id', 'section', 'title', 'date', 'file_code', 'file']
+        fields = ['id', 'section', 'title', 'date', 'file_code', 'file', 'file_name']
+
+    
+    def get_file_name(self, obj):
+        from urllib.parse import unquote
+        
+        if obj.file:
+            file_url = obj.file.url
+            filename_encoded = file_url.split("/")[-1]
+            filename = unquote(filename_encoded)
+            return filename.replace(" ", "_")
+        return None
